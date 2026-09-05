f113013f3a50436b21908d9f6f8c149804272194
Savranin-Denis <savranindenisv@gmail.com>
2026-09-05 23:05:25 +0300

    completed steps 1-4

---
+++ src/componennts/App/App.tsx
@@ -0,0 +1,29 @@
+import css from './App.module.css';
+import CafeInfo from '../CafeInfo/CafeInfo';
+import VoteOptions from '../VoteOptions/VoteOptions';
+import type { Votes } from '../../types/votes';
+import type { VoteType } from '../../types/votes';
+import { useState } from 'react';
+
+export default function App() {
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const handleVote = (type: VoteType) => {
+    setVotes({ ...votes, [type]: votes[type] + 1 });
+  };
+
+  const resetVotes = () => {
+    setVotes({ good: 0, neutral: 0, bad: 0 });
+  };
+
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+      <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={true} />
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,10 @@ import 'modern-normalize';
 import './global.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-// import App from './componennts/App/App';
+import App from './componennts/App/App';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>{/* <App /> */}</StrictMode>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
